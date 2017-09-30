5761283a890e523132068d0695e38dafce945612
Itilon <ivanov.elect@gmail.com>
2017-09-30 22:12:04 +0300

    Sammy Additional Setup

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,27 @@ import Bootstrap from 'bootstrap';
 import Sammy from 'sammy';
 
 const sammyApp = Sammy(function() {
-    this.get('', () => {
-        console.log('It Works!');
+    this.get('/', function() {
+        this.redirect('#/home');
     });
 
+    this.get('#/home', function() {
+        console.log('Home Works');
+    });
+
+    this.get('#/portfolio', function() {
+        console.log('Portfolio Works');
+    })
+
+    this.get('#/blog', function() {
+        console.log('Blog Works');
+    })
+
+    this.get('#/about', function() {
+        console.log('About Works');
+    })
+
     $(() => {
-        sammyApp.run('');
+        sammyApp.run('/');
     });
 });
