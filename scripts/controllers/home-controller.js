7db21661c8202faec9bd9bd7ac57361d95a05416
Itilon <ivanov.elect@gmail.com>
2017-10-07 13:09:25 +0300

    Footer Section Template

--- scripts/controllers/home-controller.js
+++ scripts/controllers/home-controller.js
@@ -3,15 +3,18 @@ import { data } from 'data';
 
 const $main = $('#main-section');
 const $titularSection = $('#titular-section');
+const $footer = $('#main-footer');
 
 export function getTemplate() {
     Promise.all([template.getTemplate('home'),
         template.getTemplate('slider'),
+        template.getTemplate('footer'),
         data.getPortfolio(),
         data.getQuote(),
         data.getFirstParagraph()])
-            .then(([mainTemplate, sliderTemplate, portfolio, quote, firstParagraph]) => {
+            .then(([mainTemplate, sliderTemplate, footerTemplate, portfolio, quote, firstParagraph]) => {
                 $main.html(mainTemplate([portfolio, quote, firstParagraph]));
                 $titularSection.html(sliderTemplate());
+                $footer.html(footerTemplate(firstParagraph));
             });
 };
