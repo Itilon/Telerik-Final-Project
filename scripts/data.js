47d53948ae793dd9020f918280997fe446d2fa05
Itilon <ivanov.elect@gmail.com>
2017-10-08 00:14:13 +0300

    Data Class Update

--- scripts/data.js
+++ scripts/data.js
@@ -5,26 +5,28 @@ class Data {
 
     getArticle(id) {
         id = Number(id);
-        const articleArr = $.grep(articleList, function(text) 
-            { 
-                return text.id === id;
-            } 
-        );
+        const ref = firebase.database().ref('articleList');
+        return ref.once('value')
+            .then((snapshot) => {
+                const articleList = snapshot.val();
+                const articleArr = $.grep(articleList, function(text) {
+                    return text.id === id;
+                });
 
-        const article = articleArr[0];
-
-        return article;
-    }
+                const article = articleArr[0]
+                return article;
+            });
+    };
 
     getContent() {
-        const $url = ' https://sport-vision-a49b7.firebaseio.com/aboutContent.json';
+        const $url = 'https://sport-vision-a49b7.firebaseio.com/aboutContent.json';
         return $.get($url);
-    }
+    };
 
     getFirstParagraph() {
-        const firstParagraph = aboutContent.content[0];
-        return firstParagraph;
-    }
+        const $url = 'https://sport-vision-a49b7.firebaseio.com/aboutContent/content/0.json';
+        return $.get($url);
+    };
 
     getLatestImages() {
         const latestImages = [];
@@ -33,7 +35,7 @@ class Data {
         }
 
         return latestImages;
-    }
+    };
 
     getPicture(id) {
         id = Number(id);
@@ -46,41 +48,27 @@ class Data {
         const picture = pictureArr[0];
 
         return picture;
-    }
+    };
 
     getPortfolio() {
         return portfolio;
-    }
+    };
 
     getQuote() {
-        const quoteNumber = Math.floor(Math.random() * quoteList.length);
-        const quote = quoteList[quoteNumber];
-        return quote;
-    }
-
+        const ref = firebase.database().ref('quoteList');
+        return ref.once('value')
+            .then((snapshot) => {
+                const quoteList = snapshot.val();
+                const quoteNumber = Math.floor(Math.random() * quoteList.length);
+                const quote = quoteList[quoteNumber];
+                return quote;
+            });
+    };
 }
 
 let data = new Data();
 
 export { data };
-
-const articleList = [
-    {
-        id: 0,
-        title: 'Yankees Take Control Early, Then Suffer a Crushing Defeat',
-        author: 'Billy Witz',
-        date: 'October 16, 2016',
-        category: 'Sport',
-        numberOfComments: 5,
-        content: [
-            'After Gary Sanchez and Aaron Hicks slugged home runs on Friday night off the Cleveland Indians ace Corey Kluber and helped send him to an early exit and then a five-run Yankees lead was turned over to their indomitable bullpen, they seemed destined to return to New York with their American League division series tied at a game apiece.',
-            'But the Yankees, as young and talented as they may be, learned something about the Indians’ unwavering resolve.',
-            'Francisco Lindor’s two-out, sixth-inning grand slam off Chad Green got the Indians back in the game. Jay Bruce’s solo shot off David Robertson in the eighth got them even, and Yan Gomes’s ground-ball double just inside the third-base line in the 13th lifted the Indians to a stunning 9-8 victory.',
-            'As a result, the Yankees will return home for Game 3 on Sunday facing elimination by a team that has lost just four times in its last 39 games.',
-            'It was a crushing defeat for the Yankees, who not only saw their big lead evaporate, but also failed to convert several late opportunities — including when they had pinch-runner Ronald Torreyes picked off second in extra innings with no one out. The Yankees also neglected to challenge a hit-by-pitch call, a move that might have prevented Lindor from coming to the plate and hitting his grand slam.'
-        ]
-    }
-];
 
 const portfolio = [
     { 
@@ -143,15 +131,4 @@ const portfolio = [
         url: 'https://i.ytimg.com/vi/KNe2SZ1guL8/maxresdefault.jpg',
         isLeading: false
     }
-];
-
-const quoteList = [
-    'The easiest way to make an impact in your community is just to be kind.',
-    'Kindness isn\'t about the thanks you get. It\'s about the act itself.',
-    'When there is teamwork and collaboration, wonderful things can be achieved.',
-    'Talent wins games, but teamwork wins championships.',
-    'Problems can become opportunities when the right people come together.',
-    'The greater the obstacle, the more glory in overcoming it.',
-    'We need to remember that circumstances don’t make a person, they reveal a person.',
-    'Believe in your infinite potential. Your only limitations are those you set upon yourself.'
 ];
