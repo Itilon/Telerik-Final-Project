bb803e451bc3aba7950359c08b765cc37971fc00
Itilon <ivanov.elect@gmail.com>
2017-10-08 23:31:12 +0300

    Main Navigation Responsiveness Added

--- scripts/controllers/not-found-controller.js
+++ scripts/controllers/not-found-controller.js
@@ -1,13 +1,18 @@
 import { template } from 'template';
+import { data } from 'data';
 
 const $main = $('#main-section');
-const $titularSection = $('#titular-section')
+const $titularSection = $('#titular-section');
+const $footer = $('#footer-section');
 
 export function getTemplate() {
     Promise.all([template.getTemplate('404'),
-        template.getTemplate('title')])
-        .then(([template, titleTemplate]) => {
+        template.getTemplate('title'),
+        template.getTemplate('footer'),
+        data.getFirstParagraph()])
+        .then(([template, titleTemplate, footerTemplate, firstParagraph]) => {
             $main.html(template);
             $titularSection.html(titleTemplate('404! Not Found!'));
+            $footer.html(footerTemplate(firstParagraph));
         })
 };
